a79fd8421f04cad83fa91c3bbb21a813c6feeb5c
Sanika03 <sanikasuryawanshi0305@gmail.com>
2026-05-01 17:40:26 +0530

    Centralize content on screen

--- src/components/document.tsx
+++ src/components/document.tsx
@@ -32,8 +32,8 @@ function Document({ id }: { id: string }) {
 
   return (
     <div>
-      <div>
-        <form onSubmit={updateTitle}>
+      <div className="flex max-w-6xl mx-auto justify-between pb-5">
+        <form onSubmit={updateTitle} className="flex flex-1 space-x-2">
           {/* Update title */}
           <Input value={input} onChange={(e) => setInput(e.target.value)} />
 
