afc0298d11372da395ef365c417ae00f7354a612
Sanika03 <sanikasuryawanshi0305@gmail.com>
2026-05-12 02:41:43 +0530

    Add delete document functionality

--- actions/actions.ts
+++ actions/actions.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { adminDb } from "../firebase-admin";
+import liveblocks from "@/lib/liveblocks";
 
 export async function createNewDocument() {
   const { userId, sessionClaims } = await auth();
@@ -29,4 +30,39 @@ export async function createNewDocument() {
     });
 
   return { docId: docRef.id };
+}
+
+export async function deleteDocument(roomId: string) {
+  const { userId, sessionClaims } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  try {
+    // delete the doc reference itself
+    await adminDb.collection("documents").doc(roomId).delete();
+
+    const query = await adminDb
+      .collectionGroup("rooms")
+      .where("roomId", "==", roomId)
+      .get();
+
+    const batch = adminDb.batch();
+
+    // delete the room reference in the user's collection for every user in the room
+    query.docs.forEach((doc) => {
+      batch.delete(doc.ref);
+    });
+
+    await batch.commit();
+
+    // delete the room in liveblocks
+    await liveblocks.deleteRoom(roomId);
+
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false };
+  }
 }
