44f50eb867d6522fdfc3332dde23d93af78ef42b
Sanika03 <sanikasuryawanshi0305@gmail.com>
2026-05-12 23:24:24 +0530

    Correct misplaced closing div

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -95,16 +95,18 @@ function Sidebar() {
             ))}
           </>
         )}
-      </div>
 
-      {groupedData.editor.length > 0 && (
-        <>
-          <h2 className="text-gray-500 font-semibold text-sm">Share with Me</h2>
-          {groupedData.editor.map((doc) => (
-            <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
-          ))}
-        </>
-      )}
+        {groupedData.editor.length > 0 && (
+          <>
+            <h2 className="text-gray-500 font-semibold text-sm">
+              Share with Me
+            </h2>
+            {groupedData.editor.map((doc) => (
+              <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
+            ))}
+          </>
+        )}
+      </div>
     </>
   );
 
