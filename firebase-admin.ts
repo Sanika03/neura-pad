9deff519847c60d7047b6409d6cb38cb59730fdf
Sanika03 <sanikasuryawanshi0305@gmail.com>
2026-06-11 03:07:06 +0530

    fix: load Firebase Admin credentials from environment variables for Vercel deployment

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,18 +2,19 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("./service_key.json");
-
 let app: App;
 
+const serviceKey = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string,
+);
+
 if (getApps().length === 0) {
-    app = initializeApp({
-        credential: cert(serviceKey),
-    })
+  app = initializeApp({
+    credential: cert(serviceKey),
+  });
 } else {
-    app = getApp();
+  app = getApp();
 }
 
 const adminDb = getFirestore(app);
-
 export { app as adminApp, adminDb };
